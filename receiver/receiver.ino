58b99a366047ed16d57a847d0a272c9f4938c838
Marek <marek.mikulski.14@gmail.com>
2019-06-25 09:13:42 +0200

    added testing
    dyrektywy kompilacji warunkowej załatwiają nam testy które możemy włączyć dając 1 na:
    #define TEST 1
    i wyłączyć:
    #define TEST 0
    jedną ceferką

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,5 +1,7 @@
 /* 
-    Module // Arduino UNO,NANO
+ *  Radio Controlled Car project made by Marek Mikulski
+ * 
+    moduł bezprzewodowy NRF24 -> NANO
     
     GND    ->   GND
     Vcc    ->   3.3V
@@ -12,6 +14,10 @@
 This code receive 1 channels and prints the value on the serial monitor
 */
 
+#define TEST 0  /* 1 - pozwolisz na wykonanie dodatkowych instrukcji pomagających w testach (wyświetlanie stanu na serial 
+                        monitorze itp.)
+                   0 - brak testowych instrukcji
+                */
 
 #include <SPI.h>
 #include <nRF24L01.h>
@@ -24,7 +30,7 @@ This code receive 1 channels and prints the value on the serial monitor
 #define IN4_PIN 7 // 2 silnik (sterowanie)
 
 
-const uint64_t pipeIn = 0xE8E8F0F0E1LL;     //Remember that this code is the same as in the transmitter
+const uint64_t pipeIn = 0xE8E8F0F0E1LL;     //this code has to be the same in both transmitter and receiver
 RF24 radio(9, 10);  //CSN and CE pins
 
 // The sizeof this struct should not exceed 32 bytes
@@ -42,7 +48,9 @@ Received_data received_data;
 
 void setup()
 {
+  #if TEST == 1
   Serial.begin(9600);
+  #endif
   //We reset the received values
   received_data.ch1 = 127;
   received_data.ch2 = 0;
@@ -65,7 +73,7 @@ void setup()
                                         zmienna odpowiadająca za szybkość skręcania                      */
 }
 
-/**************************************************/
+// -------------------------------------------------
 
 unsigned long last_Time = 0;
 
@@ -78,7 +86,7 @@ void receive_the_data()
 }
 }
 
-/**************************************************/
+// -------------------------------------------------
 
 void loop()
 {
@@ -88,37 +96,53 @@ void loop()
   ch1_value = map(received_data.ch1,0,255,0,255);
   ch2_value = received_data.ch2;
   if(ch2_value == 5){
+    #if TEST == 1
     Serial.println("jazda do przodu");
+    #endif
     digitalWrite(IN4_PIN, LOW);
     digitalWrite(IN3_PIN, HIGH);
   }
   else if(ch2_value == 10){
+    #if TEST == 1
     Serial.println("jazda do tyłu");
+    #endif
     digitalWrite(IN3_PIN, LOW);
     digitalWrite(IN4_PIN, HIGH);
   }
   else{
+    #if TEST == 1
     Serial.println("silnik wyłączony");
+    #endif
     digitalWrite(IN3_PIN, LOW);
     digitalWrite(IN4_PIN, LOW);
   }
   
+  #if TEST == 1
   Serial.println(ch1_value);
+  #endif
   
   if(ch1_value < 120){
+    #if TEST == 1
     Serial.println("skręca w lewo");
+    #endif
     digitalWrite(IN2_PIN, LOW);
     digitalWrite(IN1_PIN, HIGH);
   }
   else if(ch1_value > 134){
+    #if TEST == 1
     Serial.println("skręca w prawo");
+    #endif
     digitalWrite(IN1_PIN, LOW);
     digitalWrite(IN2_PIN, HIGH);
   }
   else{
+    #if TEST == 1
     Serial.println("nie skręca");
+    #endif
     digitalWrite(IN1_PIN, LOW);
     digitalWrite(IN2_PIN, LOW);
   }
-  // delay(500); // używaj delay przy obserwacji działania na serial monitorze
+  #if TEST == 1
+  delay(500); // używaj delay przy obserwacji działania na serial monitorze
+  #endif
 }
